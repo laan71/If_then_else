b21164a5ed1f23715c913e76753b3a5e09390bbe
Laila A. Andersen <lail5198@edu.easj.dk>
2020-09-28 09:36:17 +0200

    Initial commit

---
+++ src/Opgave_1.java
@@ -0,0 +1,14 @@
+public class Opgave_1 {
+
+    public static void main(String[] args) {
+
+        int salg = 500;
+        int indkøb = 325;
+        int resultat = salg - indkøb;
+
+        if (resultat > 0)
+            System.out.println("Der er overskud");
+        else
+            System.out.println("Der er underskud");
+    }
+}
